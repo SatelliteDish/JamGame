62eaf02a84428eae4a59f224b7fa9e90ef02ce1c
Michael V <michael.a.j.vitale@gmail.com>
2023-01-22 15:35:29 -0800

    Made player feel better

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -1,16 +1,15 @@
 extends KinematicBody2D
 
-export var GRAVITY = 100;
+export var GRAVITY = 10;
 export var MAXSPEED = 100;
 export var FRICTION = 150;
 export var JUMPHEIGHT = 200;
 export var SPEED = 100;
 
-var currentSpeed = 0.0;
 var velocity: Vector2 = Vector2.ZERO;
+var state: String = "IDLE";
 
 onready var stateMachine = $StateMachine;
-onready var state = "MOVE";
 
 func _ready():
 	stateMachine.connect("StateChanged",self,"on_state_changed");
@@ -22,25 +21,37 @@ func _physics_process(delta):
 		"JUMP":
 			jump_state();
 		"MOVELEFT":
-			move_state(-SPEED, delta);
+			move_left(delta);
 		"MOVERIGHT":
-			move_state(SPEED, delta);
+			move_right(delta);
 		_:
 			stateMachine.state_complete();
+	apply_friction();
+	apply_gravity();
 	move(delta);
 
-func move_state(speed: float, delta: float):
-	velocity = velocity.move_toward(Vector2(MAXSPEED, velocity.y), speed * delta);
-	
-func idle_state(delta: float):
-	velocity = velocity.move_toward(Vector2(0, velocity.y), FRICTION * delta);
+func apply_gravity():
+	velocity.y += GRAVITY;
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, FRICTION);
 
 func move(delta):
-	velocity.y += GRAVITY;
-	velocity = move_and_slide(velocity);
+	velocity = move_and_slide(velocity, Vector2.UP);
+
+
+func move_left(delta: float):
+	velocity = velocity.move_toward(Vector2(-MAXSPEED, velocity.y), SPEED * delta);
+
+func move_right(delta: float):
+	velocity = velocity.move_toward(Vector2(MAXSPEED, velocity.y), SPEED * delta);
+
+func idle_state(delta: float):
+	pass;
 
 func jump_state():
-	velocity.y -= JUMPHEIGHT; 
+	if is_on_floor():
+		velocity.y -= JUMPHEIGHT;
 	stateMachine.state_complete();
 
 func on_state_changed(_state: String):
