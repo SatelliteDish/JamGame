8c5499d7f0cf0a354fe888547e31eec9d7db9875
Michael V <michael.a.j.vitale@gmail.com>
2023-01-21 16:08:59 -0800

    Added Gravity

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -7,6 +7,7 @@ enum {
 export var ACCELERATION = 100;
 export var MAX_SPEED = 200;
 export var FRICTION = 100;
+export var GRAVITY = 100;
 
 var velocity: Vector2 = Vector2.ZERO;
 var input_vector: Vector2 = Vector2.ZERO;
@@ -22,6 +23,7 @@ func _physics_process(delta):
 			move_state(delta);
 
 func move():
+	velocity.y += GRAVITY;
 	velocity = move_and_slide(velocity);
 
 func move_state(delta):
