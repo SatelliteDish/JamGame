49ee5505238082ae4db9cbe79c1ebdea8ab365e3
Michael V <michael.a.j.vitale@gmail.com>
2023-01-22 22:21:39 -0800

    Added control randomization

--- Scripts/Entities/StateMachine.gd
+++ Scripts/Entities/StateMachine.gd
@@ -10,6 +10,9 @@ enum State {
 }
 export var BUFFERCOUNT = 1;
 export var InputHandler: NodePath = "";
+export var randomized: bool = true;
+export onready var randomMin: float = 7;
+export onready var randomMax: float = 15;
 
 onready var inputHandler = get_node(InputHandler);
 
@@ -19,11 +22,19 @@ signal StateChanged;
 
 var state = State.IDLE setget state_changed;
 var queue = [];
+var xReversed: bool = false;
+var yReversed: bool = false;
 
 func _ready():
 	inputHandler.connect("InputVectorChanged",self,"input_vector_changed");
+	var timer = get_tree().create_timer(randomMin,randomMax);
+	timer.connect("timeout",self,"randomize_inputs");
 	
 func input_vector_changed(vec: Vector2):
+	if xReversed && randomized:
+		vec.x *= -1;
+	if yReversed && randomized:
+		vec.y *= -1;
 	input_vector = vec;
 	if abs(input_vector.y) == 1:
 		match input_vector.y:
@@ -65,6 +76,24 @@ func state_changed(var _state):
 			stateString = "DEAD";
 	emit_signal("StateChanged", stateString);
 	
+func randomize_inputs():
+	var length = rand_range(randomMin,randomMax);
+	var timer = get_tree().create_timer(length);
+	timer.connect("timeout",self,"randomize_inputs");
+	var rand = randi();
+	match rand % 2:
+		0:
+			toggle_x_reversed();
+		1:
+			toggle_y_reversed();
+
+func toggle_y_reversed():
+	yReversed = !yReversed;
+
+func toggle_x_reversed():
+	xReversed = !xReversed;
+
+
 func _process(delta):
 	var stateIsCancellable = (state == State.IDLE || state == State.MOVELEFT || state == State.MOVERIGHT);
 	if stateIsCancellable && queue.size() != 0:
